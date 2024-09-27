4deb7e03756d906b9c86235a329af75386864126
Nelson Lima <nelsontlima@gmail.com>
2024-09-27 04:25:05 -0300

    Adicionou graficos plotagem dos graficos das médias.

--- plot_charts.py
+++ plot_charts.py
@@ -7,6 +7,9 @@ df_cpu = pd.read_csv("./reports/cpu.csv")
 df_mem = pd.read_csv("./reports/mem.csv")
 df_cpu_time = pd.read_csv("./reports/cpu-time.csv")
 
+media_cpu = df_cpu.rolling(window=5).mean().iloc[-1]
+media_mem = df_mem.rolling(window=5).mean().iloc[-1]
+media_cpu_time = df_cpu_time.rolling(window=5).mean().iloc[-1]
 
 system("mkdir -p charts/cpu")
 for i in range(1,6):
@@ -19,6 +22,13 @@ for i in range(1,6):
         plt.savefig(f'./charts/cpu/cpu-{i}.png')
         plt.close()
 
+media_cpu.plot()
+plt.title(f"Evolução da CPU (média dos experimentos)")
+plt.xlabel("Tempo (s)")
+plt.xticks(ticks=range(len(media_cpu)))
+plt.ylabel("Uso da CPU (%)")
+plt.savefig('./charts/cpu/cpu-average.png')
+plt.close()
 
 system("mkdir -p charts/mem")
 for i in range(1,6):
@@ -26,10 +36,19 @@ for i in range(1,6):
         mem_experiment.plot()
         plt.title(f"Evolução da Memoria no Experimento {i}")
         plt.xlabel("Tempo (s)")
-        plt.xticks(ticks=range(len(cpu_experiment)))
+        plt.xticks(ticks=range(len(mem_experiment)))
         plt.ylabel("Uso da Memoria (%)")
         plt.savefig(f'./charts/mem/mem-{i}.png')
         plt.close()
+
+media_mem.plot()
+plt.title(f"Evolução da Memoria (media dos experimentos)")
+plt.xlabel("Tempo (s)")
+plt.xticks(ticks=range(len(media_mem)))
+plt.ylabel("Uso da Memoria (%)")
+plt.savefig(f'./charts/mem/mem-average.png')
+plt.close()
+
 
 system("mkdir -p charts/cpu-time")
 for i in range(1,6):
@@ -37,7 +56,15 @@ for i in range(1,6):
         cpu_time_experiment.plot()
         plt.title(f"Evolução do tempo de CPU no Experimento {i}")
         plt.xlabel("Tempo (s)")
-        plt.xticks(ticks=range(len(cpu_experiment)))
+        plt.xticks(ticks=range(len(cpu_time_experiment)))
         plt.ylabel("Tempo de CPU (s)")
         plt.savefig(f'./charts/cpu-time/cpu-time-{i}.png')
         plt.close()
+
+media_cpu_time.plot()
+plt.title(f"Evolução do tempo de CPU (em media)")
+plt.xlabel("Tempo (s)")
+plt.xticks(ticks=range(len(media_cpu_time)))
+plt.ylabel("Tempo de CPU (s)")
+plt.savefig(f'./charts/cpu-time/cpu-time-average.png')
+plt.close()
