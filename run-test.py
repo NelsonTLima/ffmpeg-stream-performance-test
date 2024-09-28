e89db3e08b66ff8f34d10945fc90757f2be0425e
NelsonTLima <nelsontlima@gmail.com>
2024-09-28 19:00:39 -0300

    fix: different dists has different success logs after building docker containers. This fix addapt to both situations.

--- run-test.py
+++ run-test.py
@@ -37,7 +37,7 @@ def server_up():
                         print(line)
                         words_in_line = line.split()
 
-                if output and words_in_line[0] == 'Successfully':
+                if (output and len(words_in_line) > 0 and (words_in_line[0] == 'Successfully' or words_in_line[0] == 'Attaching')):
                         rtmp_status = UP
 
         while rtmp_status == UP:
